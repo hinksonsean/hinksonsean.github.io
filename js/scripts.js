85598c4e10bea0bf9ebaff5f051e33daed354d07
hinksonsean <60484058+hinksonsean@users.noreply.github.com>
2020-03-05 22:04:09 -0800

    Styling and Animating

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log('Hello World');
+
+// variables
+const trigger = document.querySelector('#trigger');
+const target = document.querySelector('section');
+
+// on click
+trigger.addEventListener( 'click' , () => {
+  
+  // do this to target element
+  // toggle class name
+  target.classList.toggle('accordion');
+
+  
+});
