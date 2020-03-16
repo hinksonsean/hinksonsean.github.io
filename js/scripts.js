c0d02258ac312165c4c0e3e646e3f9e198d0e9b7
hinksonsean <60484058+hinksonsean@users.noreply.github.com>
2020-03-16 14:52:50 -0700

    Changes Made

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,45 @@
-console.log('Hello World');
+// Everything is Great :) 
+console.log('Hello Colin :) ');
 
-// variables
-const trigger = document.querySelector('#trigger');
-const target = document.querySelector('section');
-
-// on click
-trigger.addEventListener( 'click' , () => {
+/******************************
+Random Image
+******************************/
+const images = [
+    `img/rando-1.jpg`,
+    `img/rando-2.jpg`,
+    `img/rando-3.jpg`
+  ];
   
-  // do this to target element
-  // toggle class name
-  target.classList.toggle('accordion');
-
+  console.log(images);
   
+  // Get Random Number
+  let numRand;
+  let max = 3;
+  numRand = Math.floor(Math.random() * max );
+  
+  // Get element
+  const imgSrc = document.querySelector('#rando-image');
+  console.log( imgSrc );
+  
+  // Set `src` name from stored values
+  imgSrc.src = images[numRand];
+
+/******************************
+Flickity Slider/Carousel
+******************************/
+const slideContainer = document.querySelector('.carousel');
+var flkty = new Flickity( slideContainer );
+
+/******************************
+Splitting JS
+******************************/
+Splitting({
+  target: 'h1'
 });
+
+/******************************
+Light Gallery JS
+******************************/
+const galleryContainer = document.getElementById('galleryContainer');
+
+lightGallery(galleryContainer);
